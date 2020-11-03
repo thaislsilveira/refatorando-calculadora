91b6aea3417921ec7966fcf65bf4e2257bf6322e
Thaís Silveira <thais_lsilveira@hotmail.com>
2020-11-03 16:54:22 -0300

    Refatorando as funções para outro arquivo

---
+++ script.js
@@ -0,0 +1,65 @@
+function igual() {
+  document.calc.txt.value = eval(calc.txt.value);
+}
+
+function ponto() {
+  document.calc.txt.value +='.';
+}
+
+function doisZeros() {
+  document.calc.txt.value +='00';
+}
+
+function zero() {
+  document.calc.txt.value +='0';
+}
+
+function um() {
+  document.calc.txt.value +='1';
+}
+
+function dois() {
+  document.calc.txt.value +='2';
+}
+
+function tres() {
+  document.calc.txt.value +='3';
+}
+function quatro() {
+  document.calc.txt.value +='4';
+}
+function cinco() {
+  document.calc.txt.value +='5';
+}
+function seis() {
+  document.calc.txt.value +='6';
+}
+function sete() {
+  document.calc.txt.value +='7';
+}
+function oito() {
+  document.calc.txt.value +='8';
+}
+function nove() {
+  document.calc.txt.value +='9';
+}
+
+function adicao() {
+  document.calc.txt.value +='+';
+}
+
+function subtracao() {
+  document.calc.txt.value +='-';
+}
+
+function multiplicacao() {
+  document.calc.txt.value +='*';
+}
+
+function divisao() {
+  document.calc.txt.value +='/';
+}
+
+function limparCampo() {
+  document.calc.txt.value ='';
+}
