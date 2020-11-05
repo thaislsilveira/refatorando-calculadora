2e0496b75c3ab90ed0e6654018418e7b16f7592e
Ruanderson Vieira <ruanderson@accalia.io>
2020-11-05 16:05:26 -0300

    :bug: resolve math invalid

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
 function equal() {
-  document.calc.txt.value = eval(calc.txt.value);
+
+  // if(!calc.txt.value) return;
+
+  try {
+    document.calc.txt.value = eval(calc.txt.value);
+  } catch{
+    return document.calc.txt.value = 'Erro de sintaxe'
+  }
 }
 
 function point() {
-  document.calc.txt.value +='.';
+  return document.calc.txt.value += '.';
 }
 
 function twoZeros() {
-  document.calc.txt.value +='00';
+  return document.calc.txt.value += '00';
 }
 
 function zero() {
-  document.calc.txt.value +='0';
+  return document.calc.txt.value += '0';
 }
 
 function one() {
-  document.calc.txt.value +='1';
+  return document.calc.txt.value += '1';
 }
 
 function two() {
-  document.calc.txt.value +='2';
+  return document.calc.txt.value += '2';
 }
 
 function three() {
-  document.calc.txt.value +='3';
+  return document.calc.txt.value += '3';
 }
+
 function four() {
-  document.calc.txt.value +='4';
+  return document.calc.txt.value += '4';
 }
+
 function five() {
-  document.calc.txt.value +='5';
+  return document.calc.txt.value += '5';
 }
+
 function six() {
-  document.calc.txt.value +='6';
+  return document.calc.txt.value += '6';
 }
+
 function seven() {
-  document.calc.txt.value +='7';
+  return document.calc.txt.value += '7';
 }
+
 function eight() {
-  document.calc.txt.value +='8';
+  return document.calc.txt.value += '8';
 }
+
 function nine() {
-  document.calc.txt.value +='9';
+  return document.calc.txt.value += '9';
 }
 
 function sum() {
-  document.calc.txt.value +='+';
+  return document.calc.txt.value += '+';
 }
 
 function subtraction() {
-  document.calc.txt.value +='-';
+  return document.calc.txt.value += '-';
 }
 
 function multiplication() {
-  document.calc.txt.value +='*';
+  return document.calc.txt.value += '*';
 }
 
 function division() {
-  document.calc.txt.value +='/';
+  return document.calc.txt.value += '/';
 }
 
 function clearImput() {
-  document.calc.txt.value ='';
+  return document.calc.txt.value = '';
 }
-
 
 // function sin() {
 //   document.calc.txt.value = Math.sin(document.calc.txt.value);
